626255f0dcf64a7ddc6e2e5a41d6680e75ec4b93
sqrtthatall <noviytelew@gmail.com>
2025-09-06 16:08:29 +0300

    Added 5 and 6 labs

---
+++ semester_1/lab1_introduction/5_lab.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+
+//using namespace std;
+
+int main()
+{
+    int n;
+    std::cout << "n: ";
+    std::cin >> n;
+    
+    sum = 0;
+    curent_ints = 1;
+
+    for (int i = 0; i < n; i++) {
+        sum + curent_ints;
+        sum + 2
+    }
+
+    std::cout << sum << ;
+
+    return 0;
+}
---
+++ semester_1/lab1_introduction/6_lab.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+
+//using namespace std;
+
+int main()
+{
+    int n;
+    std::cout << 'Input int: ';
+    std::cin >> n;
+    
+    if (n = n[:: - 1]) {
+        std::cout << 'Polidrome';
+    }
+    else {
+        std::cout << "Not Polidrome";
+
+    }
+
+    return 0;
+}
