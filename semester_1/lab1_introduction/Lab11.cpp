3ab428543d18778094af785a9a3bc33ab4a8a62d
sqrtthatall <noviytelew@gmail.com>
2025-09-13 19:22:33 +0300

    Added 11th lab

---
+++ semester_1/lab1_introduction/Lab11.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+#include <bitset> 
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    int a, b;
+    std::cout << "Введите натуральное десятичное число: ";
+    std::cin >> a;
+
+
+    if (std::cin.fail()) {
+        std::cout << "Ошибка: введите число!" << std::endl;
+    }
+    else {
+        std::cout << "Шестнадцатеричная форма: " << std::hex << a << std::endl;
+        std::cout << "Двочиная форма: " << std::bitset<8>(a) << std::endl;
+    }
+    return 0;
+}
