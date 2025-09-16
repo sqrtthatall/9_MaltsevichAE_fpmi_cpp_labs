9e28ef3d9f05cfc861b42562aa6995f70b9aa097
sqrtthatall <noviytelew@gmail.com>
2025-09-16 17:09:21 +0300

    Add files via upload

---
+++ semester_1/lab1_introduction/lab_9.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+
+int main()
+{
+    int n;
+    int original;
+    int m = 0;
+
+    std::cout << "Input a number: ";
+    if (!(std::cin >> n)) {
+        std::cout << "Input a NUMBER!!!";
+        return 1;
+    }
+    else {
+        original = n;
+
+        while (n != 0) {
+            m = (m * 10) + (n % 10);
+            n /= 10;
+        }
+
+        if (original == m) {
+            std::cout << original << " is a palindrome.";
+        }
+        else {
+            std::cout << original << " is NOT a palindrome.";
+        }
+    }
+    return 0;
+}
+
