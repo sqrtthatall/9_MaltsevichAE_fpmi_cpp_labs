d606dc80dadbb00311306944ab0036603930a73f
sqrtthatall <noviytelew@gmail.com>
2025-09-17 11:54:16 +0300

    Added second lab into 2 dr

---
+++ semester_1/lab2_integer_arithmetics/2_2_lab_MaltsevichAE.cpp
@@ -0,0 +1,58 @@
+#include <iostream>
+
+int main()
+{
+    int number;
+    std::cout << "Input number: ";
+    std::cin >> number;
+
+    int original = number;
+
+    for (int digit = 0; digit < 10; ++digit) {
+        int count = 0;
+        int temp = original;
+
+        while (temp > 0) {
+            if (temp % 10 == digit) {
+                count++;
+            }
+            temp /= 10;
+        }
+
+        if (count > 0) {
+            std::cout << digit << " - " << count << " times" << std::endl;
+        }
+    }
+
+    int result = 0;
+    int multiplier = 1;
+    number = original;
+
+    while (number > 0) {
+        int current_digit = number % 10;
+        int count = 0;
+        int temp = original;
+
+        while (temp > 0) {
+            if (temp % 10 == current_digit) {
+                count++;
+            }
+            temp /= 10;
+        }
+
+        if (count % 2 != 0) {
+            result = current_digit * multiplier + result;
+            multiplier *= 10;
+        }
+
+        number /= 10;
+    }
+
+    if (result == 0) {
+        result = 0;
+    }
+
+    std::cout << "Result: " << result << std::endl;
+
+    return 0;
+}
