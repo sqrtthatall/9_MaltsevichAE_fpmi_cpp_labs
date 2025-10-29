390849a5db9a9a014b2bc3363a93cee060741fb9
sqrtthatall <noviytelew@gmail.com>
2025-10-29 10:09:39 +0300

    Update 2_2_lab_MaltsevichAE.cpp

--- semester_1/lab2_integer_arithmetics/2_2_lab_MaltsevichAE.cpp
+++ semester_1/lab2_integer_arithmetics/2_2_lab_MaltsevichAE.cpp
@@ -1,62 +1,63 @@
+
 #include <iostream>
 
 int main()
 {
     int number;
     std::cout << "Input number: ";
-    if(!(std::cin >> number)){
-        std::cout << "Введите число";
-            }
-    else{
-    int original = number;
-
-    for (int digit = 0; digit < 10; ++digit) {
-        int count = 0;
-        int temp = original;
-
-        while (temp > 0) {
-            if (temp % 10 == digit) {
-                count++;
-            }
-            temp /= 10;
-        }
-
-        if (count > 0) {
-            std::cout << digit << " - " << count << " times" << std::endl;
-        }
+    if (!(std::cin >> number)) {
+        std::cout << "Input a number!!!";
+        exit(0);
     }
+    else {
+        int original = number;
 
-    int result = 0;
-    int multiplier = 1;
-    number = original;
+        for (int digit = 0; digit < 10; ++digit) {
+            int count = 0;
+            int temp = original;
 
-    while (number > 0) {
-        int current_digit = number % 10;
-        int count = 0;
-        int temp = original;
-
-        while (temp > 0) {
-            if (temp % 10 == current_digit) {
-                count++;
+            while (temp > 0) {
+                if (temp % 10 == digit) {
+                    count++;
+                }
+                temp /= 10;
             }
-            temp /= 10;
+
+            if (count > 0) {
+                std::cout << digit << " - " << count << " times" << std::endl;
+            }
         }
 
-        if (count % 2 != 0) {
-            result = current_digit * multiplier + result;
-            multiplier *= 10;
+        int result = 0;
+        int multiplier = 1;
+        number = original;
+
+        while (number > 0) {
+            int current_digit = number % 10;
+            int count = 0;
+            int temp = original;
+
+            while (temp > 0) {
+                if (temp % 10 == current_digit) {
+                    count++;
+                }
+                temp /= 10;
+            }
+
+            if (count % 2 != 0) {
+                result = current_digit * multiplier + result;
+                multiplier *= 10;
+            }
+
+            number /= 10;
         }
 
-        number /= 10;
-    }
+        if (result == 0) {
+            result = 0;
+        }
 
-    if (result == 0) {
-        result = 0;
-    }
+        std::cout << "Result: " << result << std::endl;
 
-    std::cout << "Result: " << result << std::endl;
-
-    return 0;
+        return 0;
     }
 }
-
