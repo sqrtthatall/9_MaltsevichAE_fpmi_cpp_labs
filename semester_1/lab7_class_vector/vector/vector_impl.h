5c2650c91f6e44b78f5bb03090a8385575f57a63
sqrtthatall <noviytelew@gmail.com>
2025-12-16 18:39:30 +0300

    Update vector_impl.h

--- semester_1/lab7_class_vector/vector/vector_impl.h
+++ semester_1/lab7_class_vector/vector/vector_impl.h
@@ -1,5 +1,24 @@
-#pragma once
+#ifndef VECTOR_IMPL_H
+#define VECTOR_IMPL_H
+
+#include <iostream>
+#include <initializer_list>
+#include <stdexcept>
 
 class Vector {
-    // your code goes here
-};
+private:
+    int* data_ = nullptr;
+    size_t size_ = 0;
+    size_t capacity_ = 0;
+
+    void Allocate(size_t new_capacity);
+
+public:
+    Vector() = default;
+
+    explicit Vector(size_t n);
+    Vector(std::initializer_list<int> init);
+    Vector(const Vector& other);
+    Vector& operator=(const Vector& other);
+    ~Vector();
+}
