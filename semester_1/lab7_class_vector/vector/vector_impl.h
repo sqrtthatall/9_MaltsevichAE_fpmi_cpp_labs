62e40fb66faccd4fcfc9eec58e01ce2d985e11d3
sqrtthatall <noviytelew@gmail.com>
2025-12-17 18:51:01 +0300

    Update vector_impl.h

--- semester_1/lab7_class_vector/vector/vector_impl.h
+++ semester_1/lab7_class_vector/vector/vector_impl.h
@@ -21,4 +21,24 @@ public:
     Vector(const Vector& other);
     Vector& operator=(const Vector& other);
     ~Vector();
-}
+
+    void Swap(Vector& other);
+
+    int& operator[](size_t index);
+    const int& operator[](size_t index) const;
+
+    int& At(size_t index);
+    const int& At(size_t index) const;
+
+    size_t Size() const;
+    size_t Capacity() const;
+
+    void PushBack(int value);
+    void PopBack();
+    void Clear();
+    void Reserve(size_t new_capacity);
+};
+
+std::ostream& operator<<(std::ostream& os, const Vector& v);
+
+#endif 
