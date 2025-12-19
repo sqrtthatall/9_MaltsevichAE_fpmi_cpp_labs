74a357c83272a876ad62e8592486c596cfcd0266
sqrtthatall <noviytelew@gmail.com>
2025-12-19 21:37:30 +0300

    Update train.h

--- semester_1/rgr2_vector_stl/part2/train.h
+++ semester_1/rgr2_vector_stl/part2/train.h
@@ -21,5 +21,29 @@ private:
     std::time_t travelling_time_;
 
 public:
-    // your code goes here
+    Train(size_t id, TrainType type, const std::string& destination, std::time_t dispatch_time, std::time_t travelling_time)
+        : id_(id),
+          type_(type),
+          destination_(destination),
+          dispatch_time_(dispatch_time),
+          travelling_time_(travelling_time) {}
+    
+    
+    size_t GetTrainId() const {
+        return id_;
+    }
+    TrainType GetTrainType() const {
+        return type_;
+    }
+    std::string GetDestination() const {
+        return destination_;
+    }
+    std::time_t GetDispatchTime() const {
+        return dispatch_time_;
+    }
+    std::time_t GetTravellingTime() const {
+        return travelling_time_;
+    }
+
+
 };
