851fce407caadf53b4ffc25aa9b01cb5466022f6
sqrtthatall <noviytelew@gmail.com>
2025-12-22 21:04:02 +0300

    Update main.cpp

--- semester_1/rgr3_set_list_stl/part1/main.cpp
+++ semester_1/rgr3_set_list_stl/part1/main.cpp
@@ -1,9 +1,40 @@
+#include <set>
+#include <iostream>
+
+std::set<int> FinderOfPrimeNumbers(int num) {
+    if (num < 2 || isdigit(num)) throw "Invalid input!!!\n";
+    else {
+        std::set<int> MayBeePrime;
+        for (int i = 2; i <= num; ++i) {
+            MayBeePrime.insert(i);
+        }
+
+        std::set<int> Primes;
+
+        while (!MayBeePrime.empty()) {
+            int p = *MayBeePrime.begin();
+            Primes.insert(p);
+            MayBeePrime.erase(MayBeePrime.begin());
+
+            for (long long x = p * p; x <= num; x += p) {
+                MayBeePrime.erase(x);
+            }
+        }
+        return Primes;
+    }
+}
 
 
-
-int main() {
-
-    // your code goes here
-
-    return 0;
+int main()
+{
+    try {
+        std::set<int> PrimeNums = FinderOfPrimeNumbers(200);
+        std::cout << "Prime nums: ";
+        for (int nums : PrimeNums) {
+            std::cout << nums << " ";
+        }
+    }
+    catch (const char* msg) {
+        std::cerr << "Error: " << msg;
+    }
 }
