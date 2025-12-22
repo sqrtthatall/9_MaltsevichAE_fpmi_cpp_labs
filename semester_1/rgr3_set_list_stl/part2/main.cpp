813b40ee949785b50d8c38b0dccc3e7c2e8542ab
sqrtthatall <noviytelew@gmail.com>
2025-12-22 21:44:51 +0300

    Update main.cpp

--- semester_1/rgr3_set_list_stl/part2/main.cpp
+++ semester_1/rgr3_set_list_stl/part2/main.cpp
@@ -1,6 +1,72 @@
+#include <iostream>
+#include <fstream>
+#include <list>
+#include <string>
+#include <vector>
+
+struct Authtor {
+	std::string surname;
+	std::string name;
+	std::string dadyname;
+};
+
+struct Book {
+	int udk;
+	std::string title;
+	int year;
+	std::list<Authtor> authors;
+};
+
+class Library {
+private:
+	std::list<Book> Books;
 
 
-int main() {
+public:
 
-    return 0;
+	Library() = default;
+	
+	void AddBook(const Book& book) {
+		Books.push_back(book);
+	}
+	bool RemoveBook(const std::string& title) {
+
+	}
+	std::list<Book> FindBooksByName(const std::string& title) {
+
+	}
+	std::list<Book> FindBokByAuthor(const Authtor& authtor) {
+
+	}
+	bool AddBookIntoList(const std::string& title, const Authtor& authtor) {
+
+	}
+	bool RemoveAuthBooks(const std::string& title, const Authtor& authtor) {
+
+	}
+};
+
+
+
+
+
+std::list<Book> ParsingFromFile(const std::string& filename) {
+	std::ifstream fin(filename);
+	if (!fin.is_open()) throw "Could not open the file!\n";
+	if (fin.peek() == std::fstream::traits_type::eof) throw "File is empty!\n";
+	std::list<Book> books;
+	std::string line;
+	std::vector<std::string> CurrentBookInfo;
+	while (std::getline(fin, line)) {
+
+	}
+	
 }
+
+
+
+int main()
+{
+
+}
+
